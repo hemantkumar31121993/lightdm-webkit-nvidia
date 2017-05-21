244e10bd214fd003d4a95d4313ebb0a9c74829f2
Hemant Kumar <hemantkumar31121993@gmail.com>
2017-05-21 10:54:51 +0530

    Minor changes in animation button, added script for generating and cleaning the CSS files.

---
+++ css-clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm assets/css/ring-animation.css
+
+rm assets/css/slide-animation.css
---
+++ css-gen.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+python assets/css/ring-animation.py > assets/css/ring-animation.css
+
+python assets/css/slide-animation.py > assets/css/slide-animation.css
